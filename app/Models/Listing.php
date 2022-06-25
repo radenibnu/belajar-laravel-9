17ab3b842ae1546b2f0e0f27ec8ed5de6bed7a51
radenibnu <radenibnuqc@gmail.com>
2022-06-25 08:21:26 +0700

    learn seeder, factory, Models

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if ($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      //  \App\Models\User::factory(10)->create();
+      \App\Models\User::factory(5)->create();
+
+      Listing::factory(6)->create();
+      
     }
 }
